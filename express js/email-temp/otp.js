ca33269148b08f4d73e4790d5e2e3bfcb64232ce
mishradhirajmishra <dhiraj.siswa.mishra@gmail.com>
2022-02-02 14:47:14 +0530

    update

---
+++ express js/email-temp/otp.js
@@ -0,0 +1,14 @@
+module.exports =(data,sub)=> {
+   return '\
+   <div style="border: 1px solid #c5c5c5; max-width: 484px;margin: 20px auto;  padding: 10px;">\
+   <img style="width:80px;border-radius:5px;" src=" https://offersaroundus.com/assets/front-end/img/logo/email-logo.png" >\
+   <p style="float: right;font-size: 12px;font-weight: bold;max-width: 250px;margin-top: 15px" >'+sub+'</p>\
+   <hr style="border-bottom:1px solid #c5c5c5;" >\
+   <p style="font-size: 12px;margin-bottom: 7px;">Hi,</p>\
+   <p style="font-size: 12px;" >'+data+'</p> \
+   <br><p style="font-size: 12px;" >Cheers,</p>\
+   <p style="font-size: 12px;" >Team Offers Around Us</p>\
+   <p style="font-size: 12px;" > <a href=" https://offersaroundus.com/">www.offersaroundus.com </a></p>\
+   </div>\
+   ';
+  }
